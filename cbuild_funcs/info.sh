53e7228b4dc7a6d91fc7fc29f7f989ecb49cdb3c
Jefferson Junior Medeiros de Mesquita <jeffersonjr@jeffersonPC>
2026-09-08 22:22:58 -0300

    Adiciona esqueleto da função info e contagem de linhas

--- cbuild_funcs/info.sh
+++ cbuild_funcs/info.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # aqui jaz a função info que mostra informações importantes sobre o cbuild e o programa do usuário
+
+info() {
+    # encontra todos os arquivos que terminam em .c e  .h e calcula a soma de todas as linhas de código presentes nesses arquivos
+    find . -type f \( -name "*.c" -o -name "*.h" \) | xargs wc -l
+}
+
